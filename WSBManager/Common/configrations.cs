b7d5f4162ff83a3615736eaadd483912b200a82f
Nia Tomonaka <nia1012-tmnk@outlook.jp>
2019-08-18 23:28:36 +0900

    プロジェクト ファイルを追加します。

---
+++ WSBManager/Common/configrations.cs
@@ -0,0 +1,19 @@
+﻿namespace WSBManager.Configurations {
+
+	/// <summary>
+	/// Enables or disables GPU sharing.
+	/// </summary>
+	public enum VGpu {
+		Disable,
+		Default
+	}
+
+	/// <summary>
+	/// Enables or disables networking in the sandbox.
+	/// </summary>
+	public enum Network {
+		Disable,
+		Default
+	}
+
+}
---
+++ WSBManager/Models/WSBCollectionModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using Windows.Storage;
+using System.Runtime.CompilerServices;
+
+namespace WSBManager {
+
+	/// <summary>
+	/// The list of Windows Sandbox configuration model.
+	/// </summary>
+	class WSBCollectionModel : INotifyPropertyChanged {
+
+		/// <summary>
+		/// The list of Windows Sandbox configurations.
+		/// </summary>
+		public Dictionary<string, WSBModel> WSBCollection { get; private set; }
+
+		/// <summary>
+		///	The event handler to be generated after the property changes.
+		/// </summary>
+		public event PropertyChangedEventHandler PropertyChanged;
+	}
+}
